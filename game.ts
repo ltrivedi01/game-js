2d6b93006a41f9987954c2c3420e0d96f8e78d03
lomeshtrivedi <lomesh.trivedi@originenergy.com.au>
2020-10-27 10:34:32 +1100

    initial commit

---
+++ game.ts
@@ -0,0 +1,10 @@
+document.write("External file");
+// let message : string = 'Hello World';
+// console.log(message);
+
+
+// var numberOfPlayer
+// var listOfPlayerScore = []
+// var scoreHighestRank
+// var playerScore
+
